55b45de875109f7de937f50a7c2de7c2dd2e0049
SaskiaPacker <>
2023-08-18 18:10:36 +0000

    Wire up API's to google sheet and confirm access working

--- run.py
+++ run.py
@@ -9,3 +9,15 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
     ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('absent_register')
+
+
+sales = SHEET.worksheet('average_absence_for_week')
+
+data = sales.get_all_values()
+
+print(data)
